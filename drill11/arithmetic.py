ab87ee74aabce01685df437f9322c09e34ad963b
MaryamMuhammed5050 <maryammoh2050@gmail.com>
2026-09-17 19:00:39 +0100

    drill11

--- drill11/arithmetic.py
+++ drill11/arithmetic.py
@@ -5,3 +5,10 @@ def arithmetic(a, b):
 def division_details(a,b):
     dic = {"true_division" : round(a/b, 2), "floor_division" : a//b, "remainder" : a%b}
     return dic
+
+def eligibility_logic(score,attendance,completed_drill):
+    if score >= 70 and attendance >= 80 and completed_drill == True :
+        return "Eligible"
+    
+    else:
+        return "Not eligible"
