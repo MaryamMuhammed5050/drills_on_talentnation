efe2203ed771b43782731b8353cb5d7027e54ad4
MaryamMuhammed5050 <maryammoh2050@gmail.com>
2026-09-17 18:47:11 +0100

    drill11

--- drill11/arithmetic.py
+++ drill11/arithmetic.py
@@ -1,3 +1,7 @@
 def arithmetic(a, b):
     dic = {"sum" : a+b, "product" : a * b, "power" : a**b}
     return dic
+
+def division_details(a,b):
+    dic = {"true_division" : round(a/b, 2), "floor_division" : a//b, "remainder" : a%b}
+    return dic
