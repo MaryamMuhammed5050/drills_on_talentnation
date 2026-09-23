e75ce6905b2db27e9669d666e8208c0749d867da
MaryamMuhammed5050 <maryammoh2050@gmail.com>
2026-09-23 16:50:25 +0100

    drills

--- drill05/greet.py
+++ drill05/greet.py
@@ -4,3 +4,15 @@ def fix_greeting(name):
 
     new = fix_greeting("Ada")
     return new
+
+
+    def next-age(next_age):
+        age = int(next_age)
+        age = age + 1
+        return age
+
+    def safe-divide(a, b):
+        if b == 0:
+            return "Cannot divide by zero"
+        else:
+            return round(a/b, 2)
